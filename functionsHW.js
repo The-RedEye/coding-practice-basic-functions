a1bc5047bc8f22361e2fd5e645f341d3ff19c2c4
TheRedEye <setswipe@gmail.com>
2021-10-26 16:50:21 -0400

    problem 10 complete

--- functionsHW.js
+++ functionsHW.js
@@ -89,3 +89,13 @@ function findNeedle(junk){
 }
 
 // console.log(findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk']))
+
+function positiveSum(arr){
+  sum = 0
+  for(let i=0; i<arr.length; i++)
+    if (arr[i] > 0)
+      sum+= arr[i]
+  return sum
+}
+
+// console.log(positiveSum([1, -4, 7, 12]))
