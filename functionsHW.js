73973e6ebf2924a95cc598c693c8322b39d94380
TheRedEye <setswipe@gmail.com>
2021-10-26 14:07:22 -0400

    prompt 3 done

--- functionsHW.js
+++ functionsHW.js
@@ -3,3 +3,24 @@ return num**3
 }
 
 console.log(calculateCube(5));
+
+function isAVowel(char){
+  let vowels = 'AEIOU'
+  if (vowels.includes(char.toUpperCase()))
+    return true
+  
+  return false
+}
+
+console.log(isAVowel('a'))
+console.log(isAVowel('v'))
+console.log(isAVowel('E'))
+
+function getTwoLengths(str1, str2){
+let result = [];
+result.push(str1.length)
+result.push(str2.length)
+return result
+}
+
+console.log(getTwoLengths('hello', 'worldier'))
